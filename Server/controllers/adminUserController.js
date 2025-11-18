027f046b3cd5d0b8c25e7fb718f31ca8caeba2fe
PRASHANT VISHWAKARMA <112645032+prashantvhkm@users.noreply.github.com>
2025-11-18 13:49:04 +0530

    push

--- Server/controllers/adminUserController.js
+++ Server/controllers/adminUserController.js
@@ -3,5 +3,6 @@ const AdminUser = require("../model/AdminUser");
 const sign = async (req, res) => {
   try {
     const { profileImage, name, email, password, phone } = req.body;
+    
   } catch (error) {}
 };
